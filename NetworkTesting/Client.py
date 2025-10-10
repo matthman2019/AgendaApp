5d8901a51792530430181cfaab2c9d1748fce44a
matthman2019 <142703567+matthman2019@users.noreply.github.com>
2025-10-10 16:57:57 -0400

    I need to merge. I don't think I can improve current note sharing. This was from the chromebook.

--- NetworkTesting/Client.py
+++ NetworkTesting/Client.py
@@ -1,4 +1,6 @@
 import socket
 
-with socket.create_connection(("255.255.255.255", 25313)) as s:
-    s.send("We just broadcasted this!")
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.sendto("Hello there".encode(), ("255.255.255.255", 25313))
